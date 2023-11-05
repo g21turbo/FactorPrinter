a168a78c34b7e0dd8d79fbdc889cfc66a9332d94
g21turbo <dg21stew@gmail.com>
2023-11-05 13:28:16 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static void printFactors(int number) {
+
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        }
+
+    }
+
+}
